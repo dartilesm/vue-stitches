40f48a87ba766dda5b1706b6a202186bdaed51a0
Diego Artiles <diego45882@hotmail.com>
2022-02-22 18:42:06 -0300

    feat: first commit

---
+++ src/main.js
@@ -0,0 +1,40 @@
+export default {
+  install(app) {
+    const version = Number(app.version.split('.')[0])
+
+    if (version < 3) {
+      app.directive('stitches', {
+        inserted(el, binding, vNode) {
+          const stitchesCss = binding.value;
+          const props = vNode.data.attrs;
+
+          const { className } = stitchesCss(props);
+
+          const classList = className.split(" ");
+
+          classList.forEach((_class) => {
+            el.classList.add(_class);
+          });
+          
+        }
+      })
+    } else {
+      app.directive('stitches', {
+        created(el, binding, vNode) {
+          const stitchesCss = binding.value;
+          const props = vNode.props;
+      
+          const { className } = stitchesCss(props);
+      
+          const classList = className.split(" ");
+      
+          classList.forEach((_class) => {
+            el.classList.add(_class);
+          });
+          
+        }
+      })
+    }
+    
+  }
+}
