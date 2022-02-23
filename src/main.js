494e6c4bd79696a7ae395630b3655cce4553e2a3
Diego Artiles <diego45882@hotmail.com>
2022-02-22 21:18:08 -0300

    fix (compatibility): fix vue 2.x compatibility

--- src/main.js
+++ src/main.js
@@ -2,20 +2,18 @@ export default {
   install: (app) => {
     const version = Number(app.version.split('.')[0])
     if (version < 3) {
-      app.directive('stitches', {
-        inserted(el, binding, vNode) {
-          const stitchesCss = binding.value;
-          const props = vNode.data.attrs;
-
-          const { className } = stitchesCss(props);
-
-          const classList = className.split(" ");
-
-          classList.forEach((_class) => {
-            el.classList.add(_class);
-          });
-          
-        }
+      app.directive('stitches', (el, binding, vNode) => {
+        const stitchesCss = binding.value;
+        const props = vNode.data.attrs;
+    
+        const { className } = stitchesCss(props);
+    
+        const classList = className.split(" ");
+    
+        classList.forEach((_class) => {
+          el.classList.add(_class);
+        });
+        
       })
     } else {
       app.directive('stitches', (el, binding, vNode) => {
