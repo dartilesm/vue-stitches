f6adb12372c7c35fd678ce85b77460105e7edf35
Diego Artiles <diego45882@hotmail.com>
2022-02-24 10:54:30 -0300

    feat: use variants as directive arguments

--- src/main.js
+++ src/main.js
@@ -1,34 +1,20 @@
 export default {
   install: (app) => {
-    const version = Number(app.version.split('.')[0])
-    if (version < 3) {
-      app.directive('stitches', (el, binding, vNode) => {
-        const stitchesCss = binding.value;
-        const props = vNode.data.attrs;
-    
-        const { className } = stitchesCss(props);
-    
-        const classList = className.split(" ");
-    
-        classList.forEach((_class) => {
-          el.classList.add(_class);
-        });
-        
-      })
-    } else {
-      app.directive('stitches', (el, binding, vNode) => {
-        const stitchesCss = binding.value;
-        const props = vNode.props;
-    
-        const { className } = stitchesCss(props);
-    
-        const classList = className.split(" ");
-    
-        classList.forEach((_class) => {
-          el.classList.add(_class);
-        });
-        
-      })
-    }
+    app.directive("stitches", (el, binding) => {
+      if (!Array.isArray(binding.value) && !(binding.value instanceof Function)) {
+        console.error(`v-stitches expected an Array or Function but received a ${typeof binding.value}`);
+        return;
+      }
+
+      const [stitchesCss, props = {}] = Array.isArray(binding.value) ? binding.value : [binding.value];
+
+      const { className } = stitchesCss(props);
+
+      const classList = className.split(" ");
+
+      classList.forEach((_class) => {
+        el.classList.add(_class);
+      });
+    });
   }
 }
